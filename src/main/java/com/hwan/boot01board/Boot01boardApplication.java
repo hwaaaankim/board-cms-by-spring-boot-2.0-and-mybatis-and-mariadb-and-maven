ae36f6c60a9af914d48f4873a38d763ab7dc0b8e
HwanKim <thramania@hanmail.net>
2020-10-21 10:39:43 +0900

    프로젝트 생성 및 기본적인 세팅 완료

---
+++ src/main/java/com/hwan/boot01board/Boot01boardApplication.java
@@ -0,0 +1,13 @@
+package com.hwan.boot01board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot01boardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot01boardApplication.class, args);
+	}
+
+}
