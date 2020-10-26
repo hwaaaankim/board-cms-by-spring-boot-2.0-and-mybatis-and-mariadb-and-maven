967ad839864344648b3110c5eb2f7099137af45c
HwanKim <thramania@hanmail.net>
2020-10-26 16:39:43 +0900

    ArticleDaoImpl을 제거하고 마이바티스가 대신 구현하도록 처리

--- src/main/java/com/hwan/boot01board/dao/ArticleDao.java
+++ src/main/java/com/hwan/boot01board/dao/ArticleDao.java
@@ -2,8 +2,13 @@ package com.hwan.boot01board.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.hwan.boot01board.dto.Article;
 
+@Mapper 
+// 이렇게 해주면 ArticleDao의 구현체를 마이바티스가 대신 구현 해준다
+// ArticleDaoImpl을 만들 필요가 없음
 public interface ArticleDao {
 
 	public List<Article> getList();
--- src/main/java/com/hwan/boot01board/dao/ArticleDaoImpl.java
+++ src/main/java/com/hwan/boot01board/dao/ArticleDaoImpl.java
@@ -1,5 +1,6 @@
 package com.hwan.boot01board.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
@@ -11,6 +12,17 @@ public class ArticleDaoImpl implements ArticleDao{
 	
 	@Override
 	public List<Article> getList() {
-		return null;
+		
+		List<Article> list = new ArrayList<>();
+		
+		Article article01 = new Article(1, "2020-08-08 12:12:12", "제목01", "내용01");
+		Article article02 = new Article(2, "2020-08-08 12:12:12", "제목02", "내용02");
+		Article article03 = new Article(3, "2020-08-08 12:12:12", "제목03", "내용03");
+		
+		list.add(article01);
+		list.add(article02);
+		list.add(article03);
+		
+		return list;
 	}
 }
